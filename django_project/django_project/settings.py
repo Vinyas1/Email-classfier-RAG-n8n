45dc46752e5eb6a7a2ea11cbfbc2d4e7ee366849
vinyas1 <vinyask193@gmail.com>
2026-05-25 23:13:50 -0400

    Add Django settings section 5/8

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -62,3 +62,19 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'django_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
